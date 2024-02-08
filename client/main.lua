8f163b4218cd7f6f8d59a7b232b48e7835c957c1
Renald <renald.dubus@gmail.com>
2024-02-08 18:52:22 +0100

    Revert exports

--- client/main.lua
+++ client/main.lua
@@ -1,8 +1,7 @@
-local qbrCore = exports['qbr-core']
 
 RegisterCommand('rangergetblips', function ()
-    qbrCore:TriggerCallback('sunny-job-rangerpark:server:getBlips', function(blips)
-        --qbrCore:Notify(9, 'Blips count: ' .. blips.length, 'info', 10000)
-        qbrCore:Notify(1, 'Blips OK', 'info', 10000)
+    exports['qbr-core']:TriggerCallback('sunny-job-rangerpark:server:getBlips', function(blips)
+        --exports['qbr-core']:Notify(9, 'Blips count: ' .. blips.length, 'info', 10000)
+        exports['qbr-core']:Notify(1, 'Blips OK', 'info', 10000)
     end)
 end, false)
