08e00ecdea63d1ea1de92c8c9441898d4122aa26
Renald <renald.dubus@gmail.com>
2024-02-08 16:54:32 +0100

    Test notify

--- client/main.lua
+++ client/main.lua
@@ -1,6 +1,8 @@
+local qbrCore = exports['qbr-core']
 
 RegisterCommand('rangergetblips', function ()
-    exports['qbr-core']:TriggerCallback('sunny-job-rangerpark:server:getBlips', function(blips)
-        exports['qbr-core']:Notify(9, 'Blips count: ' .. blips.length, 'info', 10000)
+    qbrCore:TriggerCallback('sunny-job-rangerpark:server:getBlips', function(blips)
+        --qbrCore:Notify(9, 'Blips count: ' .. blips.length, 'info', 10000)
+        qbrCore:Notify(9, 'Blips OK', 'info', 10000)
     end)
 end, false)
--- server/main.lua
+++ server/main.lua
@@ -4,7 +4,12 @@ exports['qbr-core']:CreateCallback('sunny-job-rangerpark:server:getBlips', funct
     local src = source
     local Player = exports['qbr-core']:GetPlayer(src)
 
-    local blips = Config.Blips:filter(function (x) return x.city == 'rhodes' end)
+    --local blips = Config.Blips:filter(function (x) return x.city == 'rhodes' end)
+    local blips = {
+        { city = 'rhodes', coords = vector3(1322.6829, -1321.438, 77.888961) },
+        { city = 'blackwater', coords = vector3(-765.0071, -1291.379, 43.834983) },
+        { city = 'valentine', coords = vector3(-326.2099, 797.40667, 121.51245) }
+    }
 
     cb(blips)
 end)
