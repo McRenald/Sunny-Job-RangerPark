c5494e58809fa899332ab92eaa0555f41fb0f331
Renald <renald.dubus@gmail.com>
2024-02-09 08:35:17 +0100

    remove blips command

--- client/main.lua
+++ client/main.lua
@@ -1,20 +1,21 @@
 
 RegisterCommand('blips', function ()
     exports['qbr-core']:TriggerCallback('sunny-job-rangerpark:server:getBlips', function(blips)
-        for k,v in pairs(blips) do
-            print(k, v["city"], v["coords"])
+        for key, value in pairs(blips) do
+            print(key, value.id, value.city, value.coords)
             /*local blip = Citizen.InvokeNative(0x554D9D53F696D002, 1664425300, v["coords"])
             SetBlipSprite(blip, k)
             SetBlipScale(blip, 0.2)
 			Citizen.InvokeNative(0x9CB1A1623062F402, blip, v["city"])*/
-            exports['qbr-core']:CreateBlip(k, v["city"], v["coords"].x, v["coords"].y, v["coords"].z)
+            exports['qbr-core']:CreateBlip(value.id, value["city"], value["coords"].x, value["coords"].y, value["coords"].z)
         end
     end)
 end, false)
 
-RegisterCommand('message', function ()
-    SendNUIMessage({
-        hello = "world",
-        action = "showMessage"
-    })
+RegisterCommand('rblips', function ()
+    exports['qbr-core']:TriggerCallback('sunny-job-rangerpark:server:getBlips', function(blips)
+        for key, value in pairs(blips) do
+            exports['qbr-core']:DeleteBlip(value.id)
+        end
+    end)
 end, false)
