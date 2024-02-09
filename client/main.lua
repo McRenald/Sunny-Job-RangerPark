9fc31744fcfc9b93544bc069cd67903a9f8b33e8
Renald <renald.dubus@gmail.com>
2024-02-09 08:24:14 +0100

    Use core fct

--- client/main.lua
+++ client/main.lua
@@ -1,29 +1,20 @@
 
 RegisterCommand('blips', function ()
     exports['qbr-core']:TriggerCallback('sunny-job-rangerpark:server:getBlips', function(blips)
-        --print(blips)
-        --print(Dump(blips))
-
         for k,v in pairs(blips) do
             print(k, v["city"], v["coords"])
-            local ClothingBlip = Citizen.InvokeNative(0x554D9D53F696D002, 1664425300, v["coords"])
-            SetBlipSprite(ClothingBlip, 1195729388)
-            SetBlipScale(ClothingBlip, 0.2)
-			Citizen.InvokeNative(0x9CB1A1623062F402, ClothingBlip, v["city"])
+            /*local blip = Citizen.InvokeNative(0x554D9D53F696D002, 1664425300, v["coords"])
+            SetBlipSprite(blip, k)
+            SetBlipScale(blip, 0.2)
+			Citizen.InvokeNative(0x9CB1A1623062F402, blip, v["city"])*/
+            exports['qbr-core']:CreateBlip(k, v["city"], v["coords"].x, v["coords"].y, v["coords"].z)
         end
     end)
 end, false)
 
-function Dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. Dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
-end
-
+RegisterCommand('message', function ()
+    SendNUIMessage({
+        hello = "world",
+        action = "showMessage"
+    })
+end, false)
