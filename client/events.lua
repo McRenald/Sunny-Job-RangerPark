2245ea91fcc008bc82f410d89c97c695ec9cdb9c
Renald <renald.dubus@gmail.com>
2024-02-11 19:31:49 +0100

    Add animation

--- client/events.lua
+++ client/events.lua
@@ -1,3 +1,4 @@
+local dict = "mech_ransack@shelf@h150cm@d80cm@reach_up@pickup@vertical@right_50cm@a"
 
 -- Events
 RegisterNetEvent("sunny-job-rangerpark:client:next-location", function ()
@@ -20,12 +21,25 @@ RegisterNetEvent("sunny-job-rangerpark:client:processing", function(location)
             DeleteLocationPrompt(location)
         end)
 
+        CreateThread(function()
+            local ped = PlayerPedId()
+            RequestAnimDict(dict)
+            while not HasAnimDictLoaded(dict) do
+                Wait(0)
+            end
+            TaskPlayAnim(ped, dict, "enter_rf", 8.0, 8.0, -1, 1, 0, false, false, false)
+		    TaskPlayAnim(ped, dict, "base", 8.0, 8.0, -1, 1, 0, false, false, false)
+        end)
+
         QbrCore:Progressbar("rangerpark_process_tree", "Traitement", 2000, false, false, {
             disableMovement = true,
             disableCarMovement = true,
             disableMouse = true,
             disableCombat = true
         }, {}, {}, {}, function() -- Done
+            RemoveAnimDict(dict)
+            Wait(700)
+		    ClearPedTasks(ped)
             QbrCore:Notify(4, "Bon travail !", 5000) -- id=2 good too
             QbrCore:DeleteBlip(location.id)
             NextLocation = nil
