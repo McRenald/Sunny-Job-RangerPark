8fbaa8c08dbd478de66673cf07ad477477625e9d
Renald <renald.dubus@gmail.com>
2024-02-11 14:21:18 +0100

    Split files

--- client/client.lua
+++ client/client.lua
@@ -26,52 +26,6 @@ CreateThread(function()
     end
 end)
 
--- Commands
-RegisterCommand('rpnext', function ()
-    if NextLocation == nil then
-        QbrCore:TriggerCallback('sunny-job-rangerpark:server:getNextLocation', function(nextLocation)
-            print('nextLocation', nextLocation.id)
-            NextLocation = nextLocation
-            QbrCore:CreateBlip(nextLocation.id, nextLocation.city, nextLocation.coords.x, nextLocation.coords.y, nextLocation.coords.z, GetHashKey(RunBlipType))
-        end)
-    end
-end, false)
-
-RegisterCommand('rpclear', function ()
-    if NextLocation ~= nil then
-        QbrCore:DeleteBlip(NextLocation.id)
-        DeleteLocationPrompt(NextLocation)
-        NextLocation = nil
-    else
-        QbrCore:TriggerCallback('sunny-job-rangerpark:server:getLocations', function(locations)
-            for key, value in pairs(locations) do
-                QbrCore:DeleteBlip(value.id)
-                QbrCore:deletePrompt(value.id)
-            end
-        end)
-    end
-    LocationPrompt = false
-end, false)
-
--- Events
-RegisterNetEvent('sunny-job-rangerpark:client:processing', function(location)
-    IsProcessing = true
-
-    DeleteLocationPrompt(location)
-
-    QbrCore:Progressbar("hospital_revive", "Traitement", 5000, false, true, {
-        disableMovement = true,
-        disableCarMovement = true,
-        disableMouse = true,
-        disableCombat = true
-    }, {}, {}, {}, function() -- Done
-        QbrCore:Notify(4, 'Bon travail !', 5000) -- id=2 good too
-        QbrCore:DeleteBlip(location.id)
-        NextLocation = nil
-        IsProcessing = false
-    end)
-end)
-
 -- Functions
 function CreateLocationPrompt(location, text)
     QbrCore:createPrompt(location.id, location.coords, 0xF3830D8E, text, { -- [J]
