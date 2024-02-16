a03ef149ddc06e38a49c7c38ce5a279d15f5cb56
Renald Dubus <renald.dubus@gmail.com>
2024-02-16 23:39:48 +0100

    Update certificate item + Payment reward

--- server/server.lua
+++ server/server.lua
@@ -9,22 +9,32 @@ CreateThread(function()
 		offDutyPay = false,
 		grades = {
             ['0'] = {
-                name = 'Recrue',
-                payment = 5
-            },
-			['1'] = {
-                name = 'Sergent',
+                name = 'Cadet',
                 payment = 10
             },
+			['1'] = {
+                name = 'Ranger',
+                payment = 25
+            },
 			['2'] = {
-                name = 'Lieutenant',
+                name = 'Ranger-Park',
 				isboss = true,
-                payment = 20
+                payment = 30
             },
         }
     })
     -- Register Item
-    QbrCore:AddItem("ranger-park-mission-certificat", {})
+    QbrCore:AddItem("ranger-park-mission-certificate", {
+        name = "ranger-park-mission-certificate",
+        label = "Certificat",
+        description = "Certificat de passage",
+        image = "",
+        weight = 0.1,
+        type = nil,
+        unique = false,
+        useable = false,
+        combinable = false
+    })
 end)
 
 -- Get all missions
@@ -49,11 +59,11 @@ QbrCore:CreateCallback("sunny-job-rangerpark:server:apply-mission-reward", funct
     local Player = QbrCore:GetPlayer(src)
     if Player and Player.PlayerData.job then
         -- Add money in bank
-        Player.Functions.AddMoney("bank", Player.PlayerData.job.payment)
+        Player.Functions.AddMoney("bank", Config.Mission.Payment)
         -- Add item to playe inventory
-        Player.Functions.AddItem("ranger-park-mission-certificat", 1)
+        Player.Functions.AddItem("ranger-park-mission-certificate", 1)
         -- Trigger client event
-        TriggerClientEvent("sunny-job-rangerpark:client:mission-completed", Player.PlayerData.job.payment)
+        TriggerClientEvent("sunny-job-rangerpark:client:mission-completed", Config.Mission.Payment)
     end
     cb(true)
 end)
--- shared/config.lua
+++ shared/config.lua
@@ -9,5 +9,6 @@ Config.Mission = {
 		{ id = "blackwater-2", office = "blackwater", type = "animal", coords = vector3(-818.792, -1258.483, 43.513) },
 		{ id = "blackwater-3", office = "blackwater", type = "tree", coords = vector3(-808.711, -1234.828, 43.395) },
 		{ id = "blackwater-4", office = "blackwater", type = "check", coords = vector3(-848.711, -1234.828, 43.395) }
-	}
+	},
+	Payment = 5
 }
